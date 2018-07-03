e4f476440bad70de222c26934e30ad021defd5de
Alexander Theißen <alex@theissen.io>
2018-07-04 00:45:18 +0200

    Added stdweb

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+#[macro_use]
+extern crate stdweb;
+
 fn main() {
-    println!("Hello, world!");
+    stdweb::initialize();
+
+    let message = "Hello, 世界!";
+    js! {
+        alert( @{message} );
+    }
+
+    stdweb::event_loop();
 }
