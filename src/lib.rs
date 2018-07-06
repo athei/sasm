aabb9fe687d883c04ef46217f64b1a2ab427a641
Alexander Theißen <alex@theissen.io>
2018-07-06 22:43:09 +0200

    Added support for engine loading

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,26 @@
 #[macro_use]
 extern crate yew;
+#[macro_use]
+extern crate stdweb;
+
+use stdweb::Value;
 
 use yew::prelude::*;
 
 pub struct Model {
-    value: u32
+    state: State,
+    simc: Option<Value>
+}
+
+enum State {
+    Unloaded,
+    Loading,
+    Idle,
+    Simulating,
 }
 
 pub enum Msg {
-    Input,
+    Button,
 }
 
 impl Component for Model {
@@ -16,10 +28,21 @@ impl Component for Model {
     type Properties = ();
 
     fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
-        Model { value: 3 }
+        Model { simc: None, state: State::Unloaded }
     }
 
     fn update(&mut self, _: Self::Message) -> ShouldRender {
+        if !self.state.button_press() {
+            return false;
+        }
+
+        match self.state {
+            State::Loading => {
+                self.simc = Some(js! { return Simc() })
+            },
+            _ => ()
+        }
+
         true
     }
 }
@@ -28,8 +51,42 @@ impl Renderable<Self> for Model {
     fn view(&self) -> Html<Self> {
         html! {
             <div>
-                    <button onclick=|_| Msg::Input,>{ "Clear Database" }</button>
+                <textarea rows="30", cols="50",>{ "BLUB" }</textarea>
+                <button disabled=self.state.button_disabled(), onclick=|_| Msg::Button,>{ self.state.button_text() }</button>
             </div>
+        }
+    }
+}
+
+impl State {
+    fn button_text(&self) -> &str {
+        match self {
+            State::Unloaded => "Load Engine",
+            State::Loading => "Loading Engine...",
+            State::Idle => "Start Simulation",
+            State::Simulating => "Simulating...",
+        }
+    }
+
+    fn button_press(&mut self) -> bool {
+        match self {
+            State::Unloaded => {
+                *self = State::Loading;
+                true
+            },
+            State::Idle => {
+                *self = State::Simulating;
+                true
+            },
+            _ => false
+        }
+    }
+
+    fn button_disabled(&self) -> bool {
+        match self {
+            State::Loading => true,
+            State::Simulating => true,
+            _ => false,
         }
     }
 }
