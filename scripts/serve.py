7340b976701eba7408f9fedced95fb1f68572b32
Alexander Theißen <alex@theissen.io>
2018-07-12 22:52:10 +0200

    Added serve.py script

---
+++ scripts/serve.py
@@ -0,0 +1,29 @@
+#!/usr/local/bin/python3
+
+import sys
+import os
+from itertools import chain
+from http import server
+from pathlib import Path
+
+# determine relevant pathes based on script location
+project_dir = Path(os.path.realpath(os.path.join(os.path.dirname(sys.argv[0]), '..')))
+static_dir = project_dir.joinpath('static')
+deploy_dir = project_dir.joinpath('target/deploy')
+build_dir = project_dir.joinpath('target/wasm32-unknown-unknown/release')
+
+# symlink build output to deploy dir
+deploy_dir.mkdir(parents=True, exist_ok=True)
+files = chain(static_dir.glob('*'),build_dir.glob('*.wasm'), build_dir.glob('*.js'))
+for f in files:
+    try:
+        os.symlink(src=f, dst=deploy_dir.joinpath(os.path.basename(f)))
+    except FileExistsError:
+        pass
+
+# serve these files from deploy directory
+print('Serving at http://localhost:8888')
+os.chdir(deploy_dir)
+server.SimpleHTTPRequestHandler.extensions_map['.wasm'] = 'application/wasm'
+httpd = server.HTTPServer(('localhost', 8888), server.SimpleHTTPRequestHandler)
+httpd.serve_forever()
